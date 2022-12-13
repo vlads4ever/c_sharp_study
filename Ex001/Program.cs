06bca55e3b8535b80f26fc02d353b36ea1755f89
vladimir_skrypnikov <sevgrad@list.ru>
2022-12-13 20:01:08 +0300

    Second changes

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,3 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World, My Friend!");
+﻿Console.WriteLine("Hello, World, My Friend!");
 Console.WriteLine("This is another note");
