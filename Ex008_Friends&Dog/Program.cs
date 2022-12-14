fbff6607a2aefa83e95cf03e71da917e7b73e9f5
vlads_geekbrains <sevgrad@list.ru>
2022-12-14 10:29:29 +0300

    Friends&Dog added

---
+++ Ex008_Friends&Dog/Program.cs
@@ -0,0 +1,27 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance/(firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance/(secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз.");
